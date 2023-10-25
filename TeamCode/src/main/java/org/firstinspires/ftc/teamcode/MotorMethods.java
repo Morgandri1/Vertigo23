d0cb8b453e2a30e316f4a384b7e2001fdaa6e8cc
DasheSmash <dashel@thepages.net>
2023-10-25 16:18:56 -0400

    Motor Setup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorMethods.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+public class MotorMethods {
+
+    public static void main(String[] args) {
+        OmniDriveMod obj = new OmniDriveMod();
+        obj.setAllSpeed(0.0,1.0,0.6,0.3);
+}
+}
